c1b7cae5d0a933640d5a763a670469855ce5cfab
Mariia Rumiantceva <mari.s70c3@gmail.com>
2019-03-16 22:35:36 +0200

    Refactoring in classes and modules

--- standalone/src/main/java/com/ifmo/wst/standalone/App.java
+++ standalone/src/main/java/com/ifmo/wst/standalone/App.java
@@ -1,5 +1,4 @@
 package com.ifmo.wst.standalone;
-//package com.wishmaster.ifmo.ws.jaxws;
 
 
 import javax.xml.ws.Endpoint;
@@ -7,6 +6,6 @@ import javax.xml.ws.Endpoint;
 public class App {
     public static void main(String[] args) {
         String url = "http://0.0.0.0:8080/StationService";
-        Endpoint.publish(url, new com.ifmo.wst.standalone.StationWebService());
+        Endpoint.publish(url, new StationWebService());
     }
 }
